0fcb023e77b8e1924035a29536073164fae8e7d8
mohammedfalaah <falahmkba@gmail.com>
2025-06-21 16:42:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,372 @@
+import Header from "./layouts/Header"
+
+
+function App() {
+
+  
+
+  return (
+    <>
+   <div>
+
+  <div className="wrappage">
+    <Header />
+  
+    {/* /header */}
+    {/* /header */}
+    <div className="container container-content">
+      <div className="collection-slide">
+        <div className="row first">
+          <div className="col-md-9 col-sm-12 col-xs-12">
+            <div className="js-slider-v2">
+              <div className="slide-img">
+                <a href="#"><img src="./public/assets/img/home9/slide_1.jpg" alt className="img-responsive" /></a>
+                <div className="box-center slide-content">
+                  <h3>Tank top<br /> hot collection</h3>
+                  <a href="#">Shop now</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-12 col-xs-12">
+            <div className="row banner">
+              <div className="col-xs-12 col-sm-6 col-md-12">
+                <div className="banner-img">
+                  <a href="#" className="effect-img3 plus-zoom">
+                    <img src="./public/assets/img/home9/banner_1.jpg" alt className="img-responsive" />
+                  </a>
+                  <div className="box-center content3">
+                    <a href="#">Women's</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-xs-12 col-sm-6 col-md-12">
+                <div className="banner-img">
+                  <a href="#" className="effect-img3 plus-zoom">
+                    <img src="./public/assets/img/home9/banner_2.jpg" alt className="img-responsive" />
+                  </a>
+                  <div className="box-center content3">
+                    <a href="#">Kid's</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="zoa-product pad4">
+      <h3 className="title text-center">featured products</h3>
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-6 col-sm-6 col-md-4 col-lg-4 product-item">
+            <div className="product-img">
+              <a href="#"><img src="./public/assets/img/home1/product_9.jpg" alt className="img-responsive" /></a>
+              <div className="product-button-group">
+                <a href="#" className="zoa-btn zoa-quickview">
+                  <span className="zoa-icon-quick-view" />
+                </a>
+                <a href="#" className="zoa-btn zoa-wishlist">
+                  <span className="zoa-icon-heart" />
+                </a>
+                <a href="#" className="zoa-btn zoa-addcart">
+                  <span className="zoa-icon-cart" />
+                </a>
+              </div>
+            </div>
+            <div className="product-info text-center">
+              <h3 className="product-title">
+                <a href="#">Grosgrain tie cotton top</a>
+              </h3>
+              <div className="product-price">
+                <span>$20.9</span>
+              </div>
+            </div>
+          </div>
+          <div className="col-xs-6 col-sm-6 col-md-4 col-lg-4 product-item">
+            <div className="product-img">
+              <a href="#"><img src="./public/assets/img/home1/product_11.jpg" alt className="img-responsive" /></a>
+              <div className="ribbon zoa-hot"><span>Hot</span></div>
+              <div className="product-button-group">
+                <a href="#" className="zoa-btn zoa-quickview">
+                  <span className="zoa-icon-quick-view" />
+                </a>
+                <a href="#" className="zoa-btn zoa-wishlist">
+                  <span className="zoa-icon-heart" />
+                </a>
+                <a href="#" className="zoa-btn zoa-addcart">
+                  <span className="zoa-icon-cart" />
+                </a>
+              </div>
+            </div>
+            <div className="product-info text-center">
+              <h3 className="product-title">
+                <a href="#">Grosgrain tie cotton top</a>
+              </h3>
+              <div className="product-price">
+                <span>$20.9</span>
+              </div>
+              <div className="color-group">
+                <a href="#" className="circle gray" />
+                <a href="#" className="circle yellow active" />
+                <a href="#" className="circle white" />
+              </div>
+            </div>
+          </div>
+          <div className="col-xs-6 col-sm-6 col-md-4 col-lg-4 product-item">
+            <div className="product-img">
+              <a href="#"><img src="./public/assets/img/home1/product_10.jpg" alt className="img-responsive" /></a>
+              <div className="ribbon zoa-sale"><span>-15%</span></div>
+              <div className="product-button-group">
+                <a href="#" className="zoa-btn zoa-quickview">
+                  <span className="zoa-icon-quick-view" />
+                </a>
+                <a href="#" className="zoa-btn zoa-wishlist">
+                  <span className="zoa-icon-heart" />
+                </a>
+                <a href="#" className="zoa-btn zoa-addcart">
+                  <span className="zoa-icon-cart" />
+                </a>
+              </div>
+            </div>
+            <div className="product-info text-center">
+              <h3 className="product-title">
+                <a href="#">Grosgrain tie cotton top</a>
+              </h3>
+              <div className="product-price">
+                <span>$20.9</span>
+              </div>
+            </div>
+          </div>
+          <div className="col-xs-6 col-sm-6 col-md-4 col-lg-4 product-item">
+            <div className="product-img">
+              <a href="#"><img src="./public/assets/img/home1/product_16.jpg" alt className="img-responsive" /></a>
+              <div className="ribbon zoa-hot"><span>Hot</span></div>
+              <div className="product-button-group">
+                <a href="#" className="zoa-btn zoa-quickview">
+                  <span className="zoa-icon-quick-view" />
+                </a>
+                <a href="#" className="zoa-btn zoa-wishlist">
+                  <span className="zoa-icon-heart" />
+                </a>
+                <a href="#" className="zoa-btn zoa-addcart">
+                  <span className="zoa-icon-cart" />
+                </a>
+              </div>
+            </div>
+            <div className="product-info text-center">
+              <h3 className="product-title">
+                <a href="#">Grosgrain tie cotton top</a>
+              </h3>
+              <div className="product-price">
+                <span>$20.9</span>
+              </div>
+            </div>
+          </div>
+          <div className="col-xs-6 col-sm-6 col-md-4 col-lg-4 product-item">
+            <div className="product-img">
+              <a href="#"><img src="./public/assets/img/home1/product_17.jpg" alt className="img-responsive" /></a>
+              <div className="ribbon zoa-new"><span>New</span></div>
+              <div className="product-button-group">
+                <a href="#" className="zoa-btn zoa-quickview">
+                  <span className="zoa-icon-quick-view" />
+                </a>
+                <a href="#" className="zoa-btn zoa-wishlist">
+                  <span className="zoa-icon-heart" />
+                </a>
+                <a href="#" className="zoa-btn zoa-addcart">
+                  <span className="zoa-icon-cart" />
+                </a>
+              </div>
+            </div>
+            <div className="product-info text-center">
+              <h3 className="product-title">
+                <a href="#">Grosgrain tie cotton top</a>
+              </h3>
+              <div className="product-price">
+                <span>$20.9</span>
+              </div>
+            </div>
+          </div>
+          <div className="col-xs-6 col-sm-6 col-md-4 col-lg-4 product-item">
+            <div className="product-img">
+              <a href="#"><img src="./public/assets/img/home1/product_18.jpg" alt className="img-responsive" /></a>
+              <div className="product-button-group">
+                <a href="#" className="zoa-btn zoa-quickview">
+                  <span className="zoa-icon-quick-view" />
+                </a>
+                <a href="#" className="zoa-btn zoa-wishlist">
+                  <span className="zoa-icon-heart" />
+                </a>
+                <a href="#" className="zoa-btn zoa-addcart">
+                  <span className="zoa-icon-cart" />
+                </a>
+              </div>
+            </div>
+            <div className="product-info text-center">
+              <h3 className="product-title">
+                <a href="#">Grosgrain tie cotton top</a>
+              </h3>
+              <div className="product-price">
+                <span>$20.9</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="text-center">
+          <a href="#" className="zoa-btn btn-loadmore">Load more</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="banner pad2">
+    <div className="container container-content">
+      <div className="row">
+        <div className="col-md-4 col-sm-4 col-xs-12">
+          <div className="banner-img">
+            <a href="#" className="effect-img3 plus-zoom">
+              <img src="./public/assets/img/home9/category-15.png" alt />
+            </a>
+            <div className="box-center content3">
+              <a href="#">Accessories</a>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4 col-sm-4 col-xs-12">
+          <div className="banner-img">
+            <a href="#" className="effect-img3 plus-zoom">
+              <img src="./public/assets/img/home9/category-16.png" alt />
+            </a>
+            <div className="box-center content3">
+              <a href="#">Summer for kids</a>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4 col-sm-4 col-xs-12">
+          <div className="banner-img">
+            <a href="#" className="effect-img3 plus-zoom">
+              <img src="./public/assets/img/home9/category-17.png" alt />
+            </a>
+            <div className="box-center content3">
+              <a href="#">Sunglass</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="container container-content">
+    <div className="zoa-instagram">
+      <div className="insta-title2 text-center">
+        <h3>INSTAGRAM</h3>
+        <a href="#">@zoa.fashion91</a>
+      </div>
+      <div className="row insta-content">
+        <div className="col-md-2 col-sm-4 col-xs-6">
+          <a href="#"><img src="./public/assets/img/home4/insta_1.jpg" alt className="img-responsive" /></a>
+        </div>
+        <div className="col-md-2 col-sm-4 col-xs-6">
+          <a href="#"><img src="./public/assets/img/home4/insta_2.jpg" alt className="img-responsive" /></a>
+        </div>
+        <div className="col-md-2 col-sm-4 col-xs-6">
+          <a href="#"><img src="./public/assets/img/home4/insta_3.jpg" alt className="img-responsive" /></a>
+        </div>
+        <div className="col-md-2 col-sm-4 col-xs-6">
+          <a href="#"><img src="./public/assets/img/home4/insta_4.jpg" alt className="img-responsive" /></a>
+        </div>
+        <div className="col-md-2 col-sm-4 col-xs-6">
+          <a href="#"><img src="./public/assets/img/home4/insta_5.jpg" alt className="img-responsive" /></a>
+        </div>
+        <div className="col-md-2 col-sm-4 col-xs-6">
+          <a href="#"><img src="./public/assets/img/home4/insta_6.jpg" alt className="img-responsive" /></a>
+        </div>
+      </div>
+    </div>
+  </div>
+  {/* Content */}
+  <div className="newsletter v3">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 col-sm-6 col-xs-12">
+          <div className="newsletter-heading">
+            <h3>get in touch</h3>
+            <p>Subscribe for latest stories and promotions (35% sale)</p>
+          </div>
+          <form className="form_newsletter" action="#" method="post">
+            <input type="email" defaultValue placeholder="Enter your emaill" name="EMAIL" id="mail" className="newsletter-input form-control" />
+            <button id="subscribe" className="button_mini zoa-btn" type="submit">
+              Subscribe
+            </button>
+          </form>
+        </div>
+        <div className="col-md-6 col-sm-6 col-xs-12">
+          <div className="newsletter-flex">
+            <div className="newsletter-element">
+              <h3>stores</h3>
+              <ul>
+                <li><a href="#">London</a></li>
+                <li><a href="#">New York</a></li>
+                <li><a href="#">Dubai</a></li>
+                <li><a href="#">Tokyo</a></li>
+              </ul>
+            </div>
+            <div className="newsletter-element">
+              <h3>shopping</h3>
+              <ul>
+                <li><a href="#">mens</a></li>
+                <li><a href="#">womens</a></li>
+                <li><a href="#">kids</a></li>
+                <li><a href="#">insta</a></li>
+              </ul>
+            </div>
+            <div className="newsletter-element">
+              <h3>stores</h3>
+              <ul>
+                <li><a href="#">about us</a></li>
+                <li><a href="#">blogs</a></li>
+                <li><a href="#">contact</a></li>
+                <li><a href="#">sitemap</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  {/* EndContent */}
+  {/* Footer */}
+  <footer className="footer v2 bg-black">
+    <div className="container">
+      <div className="f-content">
+        <div className="f-col hidden-xs hidden-sm">
+          <a href="#">
+            <img src="./public/assets/img/f-logob.png" alt className="img-responsive" />
+          </a>
+        </div>
+        <div className="f-col align-items-center">
+          <p>© 2018 <a className="red" href="#">Zoa.</a></p>
+          <ul>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Terms of Use</a></li>
+          </ul>
+        </div>
+        <div className="f-col">
+          <div className="social">
+            <a href="#"><i className="fa fa-rss" /></a>
+            <a href="#"><i className="fa fa-facebook" /></a>
+            <a href="#"><i className="fa fa-twitter" /></a>
+            <a href="#"><i className="fa fa-linkedin" /></a>
+            <a href="#"><i className="fa fa-rss" /></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  {/* End Footer */}
+</div>
+
+    </>
+  )
+}
+
+export default App
