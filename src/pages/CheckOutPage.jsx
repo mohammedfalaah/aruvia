1a68a402a9bc80256b08aa121df5db60f3a9d7ff
mohammedfalaah <falahmkba@gmail.com>
2025-08-30 21:26:22 +0530

    fix

--- src/pages/CheckOutPage.jsx
+++ src/pages/CheckOutPage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import { contextData } from '../services/Context';
 import { show_toast } from '../utils/Toast';
